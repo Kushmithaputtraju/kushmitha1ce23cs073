ff24d55fdfdeeb0a07bda23c16832babe2861c6f
Kushmithaputtraju <kushmithaputtraju3@gmail.com>
2025-09-26 09:20:17 +0530

    Add files via upload

---
+++ exceptionhandling/src/com/exception/ArrayIndex.java
@@ -0,0 +1,27 @@
+package com.exception;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+public class ArrayIndex {
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		for(int i=0;i<5;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Array:"+Arrays.toString(arr));
+		try {
+		System.out.println("5th: "+arr[5]);
+		}catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+			
+		}
+		System.out.println("program completed");
+	}
+
+}
+
+
---
+++ exceptionhandling/src/com/exception/CompileTimeException.java
@@ -0,0 +1,17 @@
+package com.exception;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class CompileTimeException {
+	public static void main(String[] args) {
+		try {
+			FileReader reader =new FileReader("abc.text");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+
